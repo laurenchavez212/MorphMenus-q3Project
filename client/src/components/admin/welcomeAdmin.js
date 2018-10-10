0dfb59a67995d80e1380a70db6523c962ee1ba0b
Lauren Chavez <laurenchavez@Laurens-MacBook-Pro.local>
2018-10-09 20:26:00 -0700

    not sure what has been added

--- client/src/components/admin/welcomeAdmin.js
+++ client/src/components/admin/welcomeAdmin.js
@@ -3,27 +3,33 @@ import { Link } from "react-router-dom";
 import "../../styles/index.css";
 import TopNav from "../homepage/topNav";
 import BottomNav from "../homepage/bottomNav";
-import {Form, Input, Label } from "reactstrap";
+import { Form, Input, Button } from "reactstrap";
 
 class Home extends Component {
   render() {
-    return <div className="adminContainer">
+    return (
+      <div className="adminContainer">
         <TopNav />
         <div>
-          <h1>Welcome Oreganos Pizza Bistro!</h1>
+                <h1>Welcome Oreganos Pizza Bistro!</h1>
+                <Button>View your menu!</Button>
           <Form>
-            <Label>Add a menu</Label>
-            <Input placeholder="Menu Name" />
+            <Input placeholder="Add a new type of Menu!" />
           </Form>
         </div>
 
         <div>
           <h3>Edit A Menu</h3>
-          <Link to={""}>Dinner</Link>
-          <Link to={""}>Drinks</Link>
+          <Button>
+            <Link to={""}>Dinner</Link>
+          </Button>
+          <Button>
+            <Link to={""}>Drinks</Link>
+          </Button>
         </div>
         <BottomNav />
-      </div>;
+      </div>
+    );
   }
 }
 export default Home;
